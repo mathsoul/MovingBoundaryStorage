a6d578193fdeb90756da8cb9acb164e05f1b57c4
Long Zhao <zhaolong.soul@gmail.com>
2015-07-10 14:12:06 -0500

    some analysis

--- debugProof.m
+++ debugProof.m
@@ -1,10 +1,24 @@
-policy1 = InitMBMPolicy();
-policy1(1:(end-1),1,:) = 1;
+InitPara()
+global A_hold b_hold A_buy b_buy A_sell b_sell 
+[A_hold,b_hold] = GenerateHoldEquation();
+[A_buy,b_buy] = GenerateBuyEquation();
+[A_sell,b_sell] = GenerateSellEquation();
 
-[value_function1,~,~,~] = SolvePDE3D(policy1);
+
+policy1 = InitMBMPolicy();
+policy1(1:(20),1:5,:) = 1;
+
+[value_function1,profit_hold1,profit_buy1,profit_sell1] = SolvePDE3D(policy1);
 
 policy2 = InitMBMPolicy();
-policy2(1:(end-1),1,:) = 1;
-policy2(2:end,3:end,:) = 2;
+policy2(1:(20),1:5,:) = 1;
+% policy2(2:end,10:end,:) = 2;
+
+idx = (profit_sell1>-0.0001);
+policy2(idx) = 2;
+
+policy = policy2(:,:,1);
 
 [value_function2,~,~,~] = SolvePDE3D(policy2);
+
+diff = value_function1(:,:,1) - value_function2(:,:,1);
